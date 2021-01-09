0bcd90ec1db82c280714346a460491d520f62ac9
kostalavista <paradoxnumber7@gmail.com>
2021-01-09 08:44:36 +0300

    Changed

--- utils/getEnvParam.js
+++ utils/getEnvParam.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({path: path.join(__dirname, '../../../.env')});
 
 function getEnvParam(param) {
 	return process.env[param];
